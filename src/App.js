da1f5792fc26977f89eef7d52a79bc212340dbb3
Chen Sicong <sicong.chen@163.com>
2020-08-11 20:18:50 +0800

    [Chen Sicong]feat: add header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
+import { Layout } from 'antd';
 import './App.scss';
+import 'antd/dist/antd.css';
+import MyHeader from './compnents/header/my-header';
+
+const { Header } = Layout;
 
 class App extends Component {
   render() {
     return (
       <main className="app">
+        <Header theme={'light'} style={{ background: '#416aff' }}>
+          <MyHeader />
+        </Header>
       </main>
     );
   }
